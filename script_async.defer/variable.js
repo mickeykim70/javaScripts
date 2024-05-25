fc07066ffb4914e28909b78ae7774d6d7fcb0bd5
mickeykim70 <mickeykim70@gmail.com>
2024-05-25 17:47:24 +0900

    May 25 1600

---
+++ script_async.defer/variable.js
@@ -0,0 +1,18 @@
+'use strict'
+
+for (let i = 0; i < 11 ; i++) {
+    if (i % 2 === 0) {
+    console.log(i);
+    } else {
+    continue;
+    }
+}
+ 
+for (let i = 0; i < 11 ; i++) {
+    if (i > 8) {
+        break;
+        
+    } else {
+        console.log(i);
+    }
+}
